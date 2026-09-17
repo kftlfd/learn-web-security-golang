56abf60cbf4dba8cdde9e6968a2a1f27b2e2f94f
kftlfd <kftlfd@ex.ua>
2026-09-17 11:24:47 +0000

    verify CSRF-token

--- internal/auth/sessions/sessions.go
+++ internal/auth/sessions/sessions.go
@@ -1,6 +1,7 @@
 package sessions
 
 import (
+	"crypto/subtle"
 	"net/http"
 	"net/url"
 	"time"
@@ -37,8 +38,8 @@ func RequireWithReturnTo(responseWriter http.ResponseWriter, request *http.Reque
 	return accounts.CurrentSession{}, false, nil
 }
 
-func CSRFTokensMatch(_, _ string) bool {
-	return true
+func CSRFTokensMatch(expected, actual string) bool {
+	return subtle.ConstantTimeCompare([]byte(expected), []byte(actual)) == 1
 }
 
 func HasRecentAuthentication(current accounts.CurrentSession, now time.Time) bool {
